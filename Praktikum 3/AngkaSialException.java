1deac34aff7cc1de43a1a449af552945da90333e
Farhan <118139435+Leciali@users.noreply.github.com>
2024-03-04 11:28:41 +0700

    Add files via upload

---
+++ Praktikum 3/AngkaSialException.java
@@ -0,0 +1,10 @@
+/* File : Asersi1.java */
+/* Deskripsi : Exception usaha sendiri nih, menolak angka 13 */
+/* NIM / Nama : 24060122140144 / Farhan Hariz Abdurrahman */
+
+public class AngkaSialException extends Exception{
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena sial broo!!");
+
+    }
+}
