4a788730d3d67ebd92dea249fd17c00127fe309b
Farhan <118139435+Leciali@users.noreply.github.com>
2024-06-08 01:55:48 +0700

    Add files via upload

---
+++ Praktikum 11/LambdaList.java
@@ -0,0 +1,16 @@
+// Kelas LambdaList
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+
+        //lambda digunakan sebagai parameter
+        mahasiswaList.forEach((nama)-> System.out.println(nama));
+    }
+}
